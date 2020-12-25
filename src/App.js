3517473aef5db4e11c5bba087ff542b7381a2fce
syedhasnainrazashah <raza.syedhasnain5@gmail.com>
2020-12-25 18:49:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      Hello world from Syed Hasnain Raza Shah
     </div>
   );
 }
